283f39e34c4df14c7dd67dfcd039d1ab0ca47e80
Josh <vkd5744@naver.com>
2021-11-16 16:48:24 +0900

    Update UserRepositoryTest (Create CRUD Method)

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,10 +2,16 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -29,16 +35,67 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
-    public void read() {
+    @Test
+    public void read(Long id) {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            System.out.println("user = " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
     }
 
+    /* 사용자가 id를 입력하면 정보를 조회해주는 메소드 / 후에 Controller 에서 적용될 코드
+    @Test
+    public User read(@RequestParam Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user = " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
+
+        return user.get();
+    }
+    */
+
+    @Test
+    @Transactional // 실제 동작은 하되 DB 에서 삭제시키지는 않음
     public void update() {
 
+        // update user set account=%?
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
+    @Test
+    @Transactional // 실제 동작 후 다시 롤백 시킴 (지운 후 원상태로 복원)
+//    @DeleteMapping("/api/user") / Controller 예시
     public void delete() {
+        Optional<User> user = userRepository.findById(7L);
 
+        Assertions.assertTrue(user.isPresent()); // true or false
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(7L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); // true or false
+
+        // 확인용
+//        if(deleteUser.isPresent()){
+//            System.out.println("데이터 존재 : " + deleteUser.get());
+//        } else {
+//            System.out.println("데이터 삭제. 데이터 없음.");
+//        }
     }
 }
-
